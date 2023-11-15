1357fd75138d8a0606ef6ae95935dad7bb1f6bdc
jjin-21 <ljj1900@naver.com>
2023-11-15 14:56:34 +0900

    231115

--- back_finance_pjt/back_finance_pjt/settings.py
+++ back_finance_pjt/back_finance_pjt/settings.py
@@ -89,6 +89,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'back_finance_pjt.urls'

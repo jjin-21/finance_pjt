4bb44f48aba5fb54532d5653a5c9d424474b691e
jjin-21 <ljj1900@naver.com>
2023-11-22 11:22:31 +0900

    ljj231122

--- back_finance_pjt/anonymous_boards/models.py
+++ back_finance_pjt/anonymous_boards/models.py
@@ -4,9 +4,9 @@ from django.conf import settings
 
 # Create your models here.
 class Board(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='anonymous_boards')
     like_users = models.ManyToManyField(
-				settings.AUTH_USER_MODEL, related_name='like_boards')
+				settings.AUTH_USER_MODEL, related_name='like_anonymous_boards')
     title = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField(blank=True)
@@ -15,7 +15,7 @@ class Board(models.Model):
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='anonymous_comments')
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
     content = models.TextField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)

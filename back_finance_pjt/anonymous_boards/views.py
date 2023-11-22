b3f8b015f3aef7ec1125f08f236fbb14e32f780c
jjin-21 <ljj1900@naver.com>
2023-11-22 11:12:51 +0900

    ljj23112

--- back_finance_pjt/anonymous_boards/views.py
+++ back_finance_pjt/anonymous_boards/views.py
@@ -1,3 +1,95 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from accounts.models import User
 
+from .models import Board, Comment
+from .serializers import BoardSerializer, CommentSerializer
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def board_list(request):
+    if request.method == 'GET':
+        boards = Board.objects.all()
+        serializer = BoardSerializer(boards, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        # serializer = BoardSerializer(data=request.data, files=request.FILES)
+        serializer = BoardSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            board = serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # elif request.method == 'PUT':
+    #     serializer = BoardSerializer(boards, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def board_detail(request, board_pk):
+    board = Board.objects.get(pk=board_pk)
+
+    if request.method == 'GET':
+        serializer = BoardSerializer(board)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'DELETE':
+        board.delete()
+        return Response({"message": "삭제되었습니다."},status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == 'PUT':
+        serializer = BoardSerializer(board, data=request.data, context={'request': request}, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def board_likes(request, board_pk):
+    board = Board.objects.get(pk=board_pk)
+    if request.user in board.like_users.all():
+        board.like_users.remove(request.user)
+    else:
+        board.like_users.add(request.user)
+    serializer = BoardSerializer(board)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def comment_list(request):
+    comments = Comment.objects.all()
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def comment_create(request, board_pk):
+    board = Board.objects.get(pk=board_pk)
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(board=board, user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+# comment_detail 인자 board_pk 뻈음
+def comment_detail(request, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    if request.method == 'GET':
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response({"message": "삭제되었습니다."}, status=status.HTTP_204_NO_CONTENT)
+        
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
